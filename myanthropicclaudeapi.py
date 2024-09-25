9d946788e6ab209ba56b29b117f2da5267e17a88
MSR <104523815+MSRAKHA@users.noreply.github.com>
2024-09-25 22:47:24 +0530

    Update myanthropicclaudeapi.py

--- myanthropicclaudeapi.py
+++ myanthropicclaudeapi.py
@@ -1,8 +1,8 @@
 import streamlit as st 
 import anthropic 
-from PIL import Image
-import os
-import base64
+# from PIL import Image
+# import os
+# import base64
 st.set_page_config(page_title="Claude API Chatbot", page_icon=":tada:", layout="wide")
 st.markdown("""
 <style>
@@ -18,14 +18,14 @@ border-color: white;
 }
 </style>""",unsafe_allow_html=True)
 st.html("""<h1 style="text-align:left; color: #4CAF50;">Rakha Claude Anthropic API</h1>""")
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
-    st.write(file_details)
-    if uploaded_file.type.startswith("image"):
-        image = Image.open(uploaded_file)
+# uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+# if uploaded_file is not None:
+#     file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
+#     st.write(file_details)
+#     if uploaded_file.type.startswith("image"):
+#         image = Image.open(uploaded_file)
 
-        st.image(image, caption= 'Uploaded Image.', use_column_width=True)
+#         st.image(image, caption= 'Uploaded Image.', use_column_width=True)
 st.html("""<h7 style="text-align:left; color:#0F75BC;">Enter your prompt here:</h7>""")
 
 prompt = st.text_area("", height=250)
@@ -35,46 +35,53 @@ if st.button("Generate Response"):
         st.error("Prompt is required")
     else:
         client = anthropic.Anthropic(api_key="sk-ant-api03-U-TIINSgIqvl4IVRAxm8HsP-bTtWJKXnhL48wWgN3NWUSNhX_mayCShV8sYuHh0kU-NXU5YbIZVEy9QcP1N30A-3pEMkgAA")
-        messages = []
+        response = client.messages.create(
+             model="claude-3-5-sonnnet-20240620",
+            messages =[{"role":"user","content":prompt}],
+            max_token=4000
+        )
+          st.write(response.content[0].text)
+        
+    #     messages = []
 
-    if uploaded_file:
-       file_bytes=uploaded_file.getvalue()
-       base64_file= base64.b64encode(file_bytes).decode('utf-8')
+    # if uploaded_file:
+    #    file_bytes=uploaded_file.getvalue()
+    #    base64_file= base64.b64encode(file_bytes).decode('utf-8')
 
-    if uploaded_file.type.startswith('image'):
-        content_type = "image"
-    else:
-        content_type = "file"
-    messages.append({
-        "role": "user",
-        "content": [
-            {
-                "type": content_type,
-                "source": {
-                "type": "base64",
-                "media type": uploaded_file.type,
-                "data": base64_file
-                          }
-            }
-        ]
-    })
+    # if uploaded_file.type.startswith('image'):
+    #     content_type = "image"
+    # else:
+    #     content_type = "file"
+    # messages.append({
+    #     "role": "user",
+    #     "content": [
+    #         {
+    #             "type": content_type,
+    #             "source": {
+    #             "type": "base64",
+    #             "media type": uploaded_file.type,
+    #             "data": base64_file
+    #                       }
+    #         }
+    #     ]
+    # })
 
     
-    messages.append({
-         "role": "user",
-         "content": prompt
+    # messages.append({
+    #      "role": "user",
+    #      "content": prompt
 
-    })
+    # })
 
-    response = client.messages.create( model="claude-3-5-sonnnet-20240620",
+#     response = client.messages.create( model="claude-3-5-sonnnet-20240620",
 
-    messages=messages,
+#     messages=messages,
 
-    max_tokens=4000
+#     max_tokens=4000
 
-)
+# )
 
-    st.write(response.content[0].text)
+#     st.write(response.content[0].text)
 
 
 
